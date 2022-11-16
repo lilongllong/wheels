aaed46ec187308278e24966c457f357dfd0903dc
harkinli <harkinli@tencent.com>
2022-11-16 14:57:01 +0800

    feat: 增加hook

--- tests/utils/index.test.ts
+++ tests/utils/index.test.ts
@@ -1,8 +1,13 @@
-import { randomString } from "../../src/utils/string";
+import { randomString, formatTime } from "../../src/utils/string";
 
 describe('string test', () => {
   test('随机字符串测试', () => {
     const result = randomString();
     expect(typeof result).toEqual('string');
+  });
+  test('测试当前时间', () => {
+    const str = '2022-11-22 00:00:00';
+    const result = formatTime(new Date(str));
+    expect(result).toEqual(str);
   });
 });
