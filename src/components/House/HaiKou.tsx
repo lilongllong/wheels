01e64cbc01d18c61fba2db273b7eb8c830aa536d
lilonglong <2453085960@qq.com>
2022-12-20 12:50:00 +0800

    feat: 增加 polished 的CSS工具

--- src/components/House/HaiKou.tsx
+++ src/components/House/HaiKou.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import { clearFix, ellipsis } from 'polished';
 
 export default function HouseIndex() {
   const Title = styled.div`
-
+    ${clearFix()}
+    ${ellipsis('250px')}
   `;
   return (
     <div className='container'>
-      展示抓取的数据，目的获取关注小区的最新售价，和挂牌价变化范围
+      <Title>展示抓取的数据，目的获取关注小区的最新售价，和挂牌价变化范围</Title>
       <div className='font-bold underline'>加粗下划线</div>
     </div>
   );
