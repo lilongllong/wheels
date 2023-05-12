b811e5bb8f35003fcc3da5e302f4d22e977d4696
021872 <021872@htsc.com.cn>
2023-05-12 17:18:08 +0800

    feat: 提交代码

--- tests/components/PageHeader.test.tsx
+++ tests/components/PageHeader.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import PageHeader from '../../src/components/PageHeader';
 import { MemoryRouter } from 'react-router-dom';
